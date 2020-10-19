8270ecd465c2977e25797cc47f07738df8e844ca
sofiaele <sofiaeleftheriou13@gmail.com>
2020-10-19 16:25:48 +0300

    two silence setup and create final feature vector

--- make_figures.py
+++ make_figures.py
@@ -1,7 +1,7 @@
 from csv import reader
 import pandas as pd
 import plotly.graph_objects as go
-
+import numpy as np
 from pyAudioAnalysis import audioSegmentation as aS
 from pyAudioAnalysis import audioBasicIO as aio
 
@@ -10,7 +10,7 @@ def make_figures():
     #read data that main produced
     df = pd.read_pickle("score.pkl")
     alignment = df.loc[0,'alignment']
-    print(alignment)
+    #print(alignment)
     rec = df.loc[0, 'recall']
     pre = df.loc[0, 'precision']
     f1 = df.loc[0, 'f1']
@@ -22,15 +22,59 @@ def make_figures():
 
     #audio features
     fs, x = aio.read_audio_file("output.wav")
-    seg_limits = aS.silence_removal(x, fs, 0.006, 0.006,0.07)
-    print(seg_limits)
-    total_speech = 0.0
-    for k in seg_limits:
-        word_speech = k[1] - k[0]
-        total_speech = total_speech + word_speech
-    speech_ratio=total_speech/dur
-    total_silence=dur-total_speech
-    number_of_pauses=len(seg_limits)
+    seg_limits_short = aS.silence_removal(x, fs, 0.06, 0.06,0.07)
+    #print(seg_limits_short)
+    seg_limits_long = aS.silence_removal(x,fs,0.1,0.1,0.3)
+    #print(seg_limits_long)
+
+    total_speech_short = 0.0
+    silence_durations_short = []
+    counter=0
+    for k in seg_limits_short:
+        if counter==0:
+            silence_durations_short.append(k[0])
+        elif counter==(len(seg_limits_short)-1):
+            p = counter - 1
+            silence_durations_short.append(k[0] - seg_limits_short[p][1])
+            silence_durations_short.append(dur-k[1])
+        else:
+            p=counter-1
+            silence_durations_short.append(k[0]-seg_limits_short[p][1])
+        word_speech_short = k[1] - k[0]
+        total_speech_short = total_speech_short + word_speech_short
+        counter=counter+1
+    speech_ratio_short=total_speech_short/dur
+    number_of_pauses_short=len(silence_durations_short)
+    #print(silence_durations_short)
+
+    total_speech_long = 0.0
+    counter=0
+    silence_durations_long = []
+    for k in seg_limits_long:
+        if counter==0:
+            silence_durations_long.append(k[0])
+        elif counter==(len(seg_limits_long)-1):
+            p = counter - 1
+            silence_durations_long.append(k[0] - seg_limits_long[p][1])
+            silence_durations_long.append(dur-k[1])
+        else:
+            p=counter-1
+            silence_durations_long.append(k[0]-seg_limits_long[p][1])
+        word_speech_long = k[1] - k[0]
+        total_speech_long = total_speech_long + word_speech_long
+        counter=counter+1
+    print(seg_limits_short)
+    print(silence_durations_short)
+    print(seg_limits_long)
+    print(silence_durations_long)
+    speech_ratio_long = total_speech_long / dur
+    number_of_pauses_long = len(silence_durations_long)
+    silence_durations_short = np.array(silence_durations_short)
+    silence_durations_long = np.array(silence_durations_long)
+    std_short = np.std(silence_durations_short)
+    std_long = np.std(silence_durations_long)
+    average_silence_dur_short= np.mean(silence_durations_short)
+    average_silence_dur_long = np.mean(silence_durations_long)
 
     #take x and y coordinates
     X_Rlist = df1['x'].to_numpy()
@@ -48,19 +92,28 @@ def make_figures():
         csv_reader = reader(read_obj)
         Asr=list(csv_reader)
 
-
+    #create feature vector for the whole recording
+    DF=pd.DataFrame(
+        {
+            "average duration" : [float("{:.2f}".format(average_silence_dur_short)),float("{:.2f}".format(average_silence_dur_long))],
+            "silence_seg_per_minute" : [float("{:.2f}".format(number_of_pauses_short/(dur/60.0))),float("{:.2f}".format(number_of_pauses_long/(dur/60.0)))],
+            "std" : [float("{:.2f}".format(std_short)),float("{:.2f}".format(std_long))]
+        }
+    )
+    DF.to_csv('whole_recording_feature_vector.csv',index=False)
     fig = go.Figure(data=[go.Table(
-        columnorder=[1, 2],
-        columnwidth=[80, 400],
-        header=dict(values=[[],['<b>SCORES</b>']],
+        columnorder=[1, 2, 3],
+        columnwidth=[80, 200, 200],
+        header=dict(values=[['<b>SCORES</b>'],['<b>Short Windows</b>'],['<b>Long Windows</b>']],
                     line_color='darkslategray',
                     fill_color='royalblue',
                     align='center',
                     font=dict(color='white', size=12),
                     height=40
         ),
-        cells=dict(values=[["Recall Score (%)","Precision Score (%)","F1 Score (%)","Number of words","Total Duration (sec)","Word Rate (words/min)","Speech Ratio (sec)","Word Rate in Speech (words/sec)","Total Silence Duration (sec)","Number of Pauses"],
-                           [float("{:.2f}".format(rec)), float("{:.2f}".format(pre)), float("{:.2f}".format(f1)),words,dur,float("{:.2f}".format(words/(dur/60.0))),float("{:.2f}".format(speech_ratio)),float("{:.2f}".format(words/total_speech)),float("{:.2f}".format(total_silence)),number_of_pauses]],
+        cells=dict(values=[["Recall Score (%)","Precision Score (%)","F1 Score (%)","Number of words","Total Duration (sec)","Word Rate (words/min)","Speech Ratio (sec)","Word Rate in Speech (words/sec)","Silence Average Duration (sec)","Number of Pauses","Standard deviation of silence duration (sec)"],
+                           [float("{:.2f}".format(rec)), float("{:.2f}".format(pre)), float("{:.2f}".format(f1)),words,dur,float("{:.2f}".format(words/(dur/60.0))),float("{:.2f}".format(speech_ratio_short)),float("{:.2f}".format(words/total_speech_short)),float("{:.2f}".format(average_silence_dur_short)),number_of_pauses_short,float("{:.2f}".format(std_short))],
+                           [float("{:.2f}".format(rec)), float("{:.2f}".format(pre)), float("{:.2f}".format(f1)),words,dur,float("{:.2f}".format(words/(dur/60.0))),float("{:.2f}".format(speech_ratio_long)),float("{:.2f}".format(words/total_speech_long)),float("{:.2f}".format(average_silence_dur_long)),number_of_pauses_long,float("{:.2f}".format(std_long))]],
                    line_color='darkslategray',
                    fill=dict(color=['paleturquoise', 'white']),
                    align=['left', 'center'],
