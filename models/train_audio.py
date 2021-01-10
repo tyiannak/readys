b016b6607d3b26ffd59362af6d16d6a9f51c9dcd
sofiaele <sofiaeleftheriou13@gmail.com>
2021-01-10 20:59:38 +0200

    slight correction train_audio

--- models/train_audio.py
+++ models/train_audio.py
@@ -2,7 +2,7 @@ from pyAudioAnalysis.audioTrainTest import extract_features_and_train
 import os
 import argparse
 
-def train_svm(files_path):
+def train_svm(files_path,output_model):
     '''
     Train audio models
     :param files_path: directory which contains audio organized in folders of classes
@@ -10,12 +10,14 @@ def train_svm(files_path):
     '''
     onlydir = [x[0] for x in os.walk(files_path)]
     onlydir = onlydir[1:]
-    extract_features_and_train(onlydir,3,3,0.2,0.2,"svm_rbf","valence_svm_rbf")
+    extract_features_and_train(onlydir,3,3,0.2,0.2,"svm_rbf",output_model)
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input",
                         help="the path of directory which contains audio organized in folders of classes" )
+    parser.add_argument("-o", "--outputmodelpath",
+                        help="path to the final svm model to be saved")
     args = parser.parse_args()
-    train_svm(args.input)
+    train_svm(args.input,args.outputmodelpath)
