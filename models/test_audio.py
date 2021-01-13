ebd7c5d2dca8cf92c2463930b308e327c8e6eeb4
lobracost <pa.koromilas@gmail.com>
2021-01-13 11:53:23 +0200

    Minor refactoring on audio scripts

--- models/test_audio.py
+++ models/test_audio.py
@@ -1,16 +1,18 @@
 import argparse
-from pyAudioAnalysis.audioSegmentation import  mid_term_file_classification
+from pyAudioAnalysis.audioSegmentation import mid_term_file_classification
 
-def predict_audio_labels(audio_file,svm_model):
-    '''
+
+def predict_audio_labels(audio_file, svm_model):
+    """
     Predict audio class
     :param audio_file: path of wav file
     :param svm_model: path of saved svm classifier
-    :return: a dictionary that has as elements the classes and as values the percentage (%) that this wav belongs to each class
-    '''
-    labels,class_names,_,_ = mid_term_file_classification(audio_file,svm_model,"svm_rbf")
-    print(class_names)
-    print(labels)
+    :return: a dictionary that has as elements the classes
+        and as values the percentage (%) that this wav
+        belongs to each class
+    """
+    labels, class_names, _, _ = mid_term_file_classification(
+        audio_file, svm_model, "svm_rbf")
     num_of_samples = len(labels)
     dictionary = {}
     for label in class_names:
@@ -24,7 +26,6 @@ def predict_audio_labels(audio_file,svm_model):
     return dictionary
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input",
@@ -32,5 +33,6 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--classifier",
                         help="the path of the classifier")
     args = parser.parse_args()
-    results = predict_audio_labels(args.input,args.classifier)
-    print(results)
+    results = predict_audio_labels(args.input, args.classifier)
+    print("Predicted probability percentage "
+          "for each class:\n     {}".format(results))
--- models/train_audio.py
+++ models/train_audio.py
@@ -4,15 +4,17 @@ import argparse
 
 
 def train_svm(files_path, output_model):
-    '''
+    """
     Train audio models
-    :param files_path: directory which contains audio organized in folders of classes
+    :param files_path: directory which contains
+        audio organized in folders of classes
+    :param output_model: path to save the output model
     :return: the name of the saved model
-    '''
+    """
 
-    onlydir = [x[0] for x in os.walk(files_path)]
-    onlydir = onlydir[1:]
-    extract_features_and_train(onlydir, 3, 3,
+    dirs = [x[0] for x in os.walk(files_path)]
+    dirs = dirs[1:]
+    extract_features_and_train(dirs, 3, 3,
                                0.2, 0.2, "svm_rbf",
                                output_model)
     return
@@ -21,7 +23,9 @@ def train_svm(files_path, output_model):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input",
-                        help="the path of directory which contains audio organized in folders of classes" )
+                        help="the path of directory which "
+                             "contains audio organized "
+                             "in folders of classes")
     parser.add_argument("-o", "--outputmodelpath",
                         help="path to the final svm model to be saved")
     args = parser.parse_args()
