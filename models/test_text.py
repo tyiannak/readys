9ed6c40feb29016363dce0ce15a76088364ca671
Theodoros Giannakopoulos <tyiannak@gmail.com>
2021-01-18 14:28:23 +0200

    minor

--- models/test_text.py
+++ models/test_text.py
@@ -2,27 +2,28 @@ from models.train_text import extract_fast_text_features
 from models.train_text import load_text_embeddings
 import argparse
 import pickle as cPickle
-import re
 import pandas as pd
 
-def extract_features(data,fasttext_pretrained_model,embeddings_limit=None):
-    '''
+
+def extract_features(data, fasttext_pretrained_model, embeddings_limit=None):
+    """
     Extract features from text segments
     :param data: list of samples (text-segments)
     :param fasttext_pretrained_model:  the fast text pretrained model loaded
-    :param embeddings_limit: embeddings_limit: limit of the number of embeddings.
+    :param embeddings_limit: embeddings_limit: limit of the number of embeddings
         If None, then the whole set of embeddings is loaded.
     :return:
     -feature_matrix: features of all samples (n samples x m features)
     -num_of_samples: number of samples
-    '''
+    """
     num_of_samples = len(data)
     feature_matrix = extract_fast_text_features(
         data, fasttext_pretrained_model,
         embeddings_limit)
     return feature_matrix , num_of_samples
 
-def predict_text_labels(feature_matrix,num_of_samples, svm_model,
+
+def predict_text_labels(feature_matrix, num_of_samples, svm_model,
                         classes_file):
     """
     segment-level classification of text to classify into aggregated classes
@@ -36,7 +37,6 @@ def predict_text_labels(feature_matrix,num_of_samples, svm_model,
     -predicted_labels : a list of predicted labels of all samples
     """
 
-    print(feature_matrix.shape)
     with open(svm_model, 'rb') as fid:
         classifier = cPickle.load(fid)
         mean = cPickle.load(fid)
@@ -71,14 +71,17 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--names",
                         help="the path of csv file that contains the"
                               " name of classes of this specific model")
-    parser.add_argument('-l', '--embeddings_limit', required=False, default=None, type=int,
+    parser.add_argument('-l', '--embeddings_limit', required=False,
+                        default=None, type=int,
                         help='Strategy to apply in transfer learning: 0 or 1.')
 
     args = parser.parse_args()
 
     text_embed_model = load_text_embeddings(args.pretrained,
                                             args.embeddings_limit)
-    feature_matrix , num_of_samples = extract_features(args.input,text_embed_model,args.embeddings_limit)
+    feature_matrix , num_of_samples = extract_features(args.input,
+                                                       text_embed_model,
+                                                       args.embeddings_limit)
     results = predict_text_labels(feature_matrix,num_of_samples,
                                   args.classifier, args.names)
     print(results)
