90770b5a8f1dbb03f32c1c7dd489c4466f272618
sofiaele <sofiaeleftheriou13@gmail.com>
2021-02-10 15:54:06 +0200

    import changes

--- models/test_audio.py
+++ models/test_audio.py
@@ -1,7 +1,13 @@
 import argparse
 import pickle5 as pickle
 from collections import Counter
-from feature_extraction import AudioFeatureExtraction
+import os
+import sys
+
+sys.path.insert(0, os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '../'))
+
+from models.feature_extraction import AudioFeatureExtraction
 
 
 def predict_audio_labels(filename, classifier_path):
