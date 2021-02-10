42a956ac0d41dd60ae30ec9cefcb689e30881d3e
sofiaele <sofiaeleftheriou13@gmail.com>
2021-02-10 15:53:12 +0200

    make paths run from parent directory

--- models/recording_level_feature_extraction.py
+++ models/recording_level_feature_extraction.py
@@ -93,7 +93,8 @@ class RecordingLevelFeatureExtraction(object):
         segmentation_threshold = self.basic_features_params['text_segmentation_params']['segmentation_threshold']
         method = self.basic_features_params['text_segmentation_params']['method_of_segmentation']
         overall_features = []
-
+        audio_models_directory = 'models/' + audio_models_directory
+        text_models_directory = 'models/' + text_models_directory
         #load text classifiers attributes containing embeddings in order not to be loaded for every sample
         classifiers_attributes = []
         for filename in os.listdir(text_models_directory):
