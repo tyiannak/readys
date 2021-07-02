323a285a577ca9ed13c35bd8f283a783a83bf9f7
sofiaele <sofiaeleftheriou13@gmail.com>
2021-07-02 19:02:51 +0300

    test recording level classifier

--- models/test_recording_level.py
+++ models/test_recording_level.py
@@ -14,7 +14,7 @@ from models.recording_level_feature_extraction \
 import argparse
 import yaml
 import os
-
+import numpy as np
 
 script_dir = os.path.dirname(__file__)
 if not script_dir:
@@ -26,30 +26,33 @@ else:
 
 config = conf['recording_level_classifier']
 
-def predict_recording_level_label(audio_file, model_path):
+def predict_recording_level_label(audio_file, model1, model2=None):
     """
     :param audio_file: the path of audio file to test
     :param model_path: the path of the recording level model to use
     :return: class_name : the predicted class of the input file
     """
-
-    if not os.path.isfile(model_path):
-        print("mtFileClassificationError: input model_type not found!")
-        return
     basic_features_params = {}
     basic_features_params['google_credentials'] = config['google_credentials']
     basic_features_params['audio_models_folder'] = config['audio_models_folder']
     basic_features_params['text_models_folder'] = config['text_models_folder']
 
-    model_dict = pickle.load(open(model_path, 'rb'))
-    basic_features_params['features_type'] = model_dict['features_type']
-    basic_features_params['reference_text'] = model_dict['reference_text']
-    basic_features_params['text_segmentation_params'] =\
-        model_dict['text_segmentation_params']
+
+    model_dict = pickle.load(open(model1, 'rb'))
+
+    basic_features_params['classifier_type'] = model_dict['classifier_type']
+    basic_features_params['class_mapping'] = model_dict['class_mapping']
+    basic_features_params['classifier'] = model_dict['classifier']
+    basic_features_params['features_type']  = model_dict['features_type']
+    basic_features_params['reference_text']  = model_dict['reference_text']
+    basic_features_params['text_segmentation_params'] = model_dict['text_segmentation_params']
     basic_features_params['audio_features'] = model_dict['audio_features']
     basic_features_params['pyaudio_params'] = model_dict['pyaudio_params']
     basic_features_params['pyaudio_num_features'] = model_dict['pyaudio_num_features']
+    basic_features_params['raudio_num_features_discard']  = model_dict['raudio_num_features_discard']
     basic_features_params['gender'] = model_dict['gender']
+
+
     #feature_extraction
     feature_extractor = RecordingLevelFeatureExtraction(basic_features_params)
     if basic_features_params['reference_text']:
@@ -60,16 +63,36 @@ def predict_recording_level_label(audio_file, model_path):
         textfile = [os.path.join(folder, file_name)]
     else:
         textfile = []
-    feature_matrix, _ , _, _, _, _, _ = \
-        feature_extractor.extract_recording_level_features([audio_file],
-                                                           textfile)
-
+    fused_features, fused_names, readys_features, readys_names, pyaudio_features, pyaudio_names, labels, filenames= \
+        feature_extractor.extract_recording_level_features([audio_file],textfile,['positive'])
+    print(readys_names)
+    print(readys_features)
+    print(pyaudio_names)
+    print(pyaudio_features)
     classifier = model_dict['classifier']
     class_mapping = model_dict['class_mapping']
-    class_id = classifier.predict(feature_matrix)
-    class_id = int(class_id)
-    class_name = class_mapping[class_id]
-    return class_name
+    if basic_features_params['audio_features'] == "fused":
+        class_id = classifier.predict(fused_features)
+        class_id = int(class_id)
+        class_name = class_mapping[class_id]
+    elif basic_features_params == "audio":
+        class_id = classifier.predict(readys_features)
+        class_id = int(class_id)
+        class_name = class_mapping[class_id]
+    else:
+        proba1 = classifier.predict_proba(readys_features)
+        model_dict2 = pickle.load(open(model2, 'rb'))
+        classifier2 = model_dict2['classifier']
+        proba2 = classifier2.predict_proba(pyaudio_features)
+        average = np.mean([proba1, proba2], axis=0)
+        y_pred = np.argmax(average, axis=1)
+        class_id = int(y_pred)
+        class_name = class_mapping[class_id]
+    print("class name:",class_name)
+    print("class id:",class_id)
+    return
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -78,7 +101,8 @@ if __name__ == '__main__':
     parser.add_argument("-m", "--model_path", required=True,
                         help="the path of the model that "
                              "we are gonna use to test")
+    parser.add_argument("-m2", "--model2_path", required=False, default='None',
+                        help="the path of the pyaudio model if we use late fusion")
 
     args = parser.parse_args()
-    class_name = predict_recording_level_label(args.input, args.model_path)
-    print(class_name)
+    predict_recording_level_label(args.input, args.model_path, args.model2_path)
